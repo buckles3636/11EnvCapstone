f0644fe73befb20a2524fa98e54367e3bb4d7b87
unknown <sethhoward2117@gmail.com>
2025-01-27 16:05:40 -0700

    Added sensor code to David's template

--- software/subsystems/sense.py
+++ software/subsystems/sense.py
@@ -1,7 +1,13 @@
 import multiprocessing as mp
 import time
+import argparse
 
+# pip install sensirion 
 from subsystems.subsystem import Subsystem
+from sensirion_driver_adapters.i2c_adapter.i2c_channel import I2cChannel
+from sensirion_i2c_driver import LinuxI2cTransceiver, I2cConnection, CrcCalculator
+from sensirion_i2c_sht4x.device import Sht4xDevice
+from sensirion_i2c_stc3x.device import Stc3xDevice
 
 class Sensor(Subsystem):
 
@@ -36,9 +42,35 @@ class Sensor(Subsystem):
           # initialize the logger
           self.logger = mp.log_to_stderr()
 
-          # create any necessary custom classes for functionality
-          self.class_1 = Class1()
-          self.class_2 = Class2()
+          # Initialize argument parser for I2C bus setup
+          parser = argparse.ArgumentParser()
+          parser.add_argument('--i2c-port', '-p', default='/dev/i2c-1')
+          args = parser.parse_args()
+
+          # Set up I2C bus for the STC31 and SHT40
+          with LinuxI2cTransceiver(args.i2c_port) as i2c_transceiver:
+          i2c_transceiver = I2cConnection(i2c_transceiver)
+          stc31_channel = I2cChannel(i2c_transceiver,
+                                        slave_address=0x29,
+                                        crc=CrcCalculator(8, 0x31, 0xff, 0x0))
+          sht40_channel = I2cChannel(i2c_transceiver,
+                                        slave_address=0x44,
+                                        crc=CrcCalculator(8, 0x31, 0xff, 0x0))
+          self.stc31_sensor = Stc3xDevice(stc31_channel)
+          self.sht40_sensor = Sht4xDevice(sht40_channel)
+          time.sleep(0.014)
+
+          # Output serial number for SHT40
+          sht40_serial_number = sht40_sensor.serial_number()
+          logger.info(f"SHT4x Serial Number = {sht40_serial_number}")
+
+          # Output the product identifier and serial number for STC31
+          (stc31_product_id, stc31_serial_number) = stc31_sensor.get_product_id()
+          print(f"STC3x Product id = {stc31_product_id}")
+          print(f"STC3x Serial Number = {stc31_serial_number}")
+
+          # Measure STC31-C CO2 in air in range 0% - 25%
+          stc31_sensor.set_binary_gas(19)
 
      def start(self) -> None:
           # override the parent start() function
@@ -46,14 +78,23 @@ class Sensor(Subsystem):
           # pipes can be assessed like the following: self.pipe_sensor_data_in.send(<data_here>)
           # data packets can be created like the following: data_dict = {"CO2": 5.1, "temperature": 37.0, "humidity": 90.0}
           while True:
-              time.sleep(2)
-              self.pipe_sensor_data_out.send({"CO2": 10.0, "temperature": 10.0, "humidity": 10.0})
-              self.logger.info("data sent")
+               # Set the sampling to 1Hz
+               time.sleep(1.0)
 
-# custom class
-class Class1():
-    pass
+               # Read humidity and temperature from SHT40 sensor and use
+               # it for CO2 measurement compensation.
+               sht40_temperature, sht40_humidity = self.sht40_sensor.measure_high_precision()
 
-# custom class
-class Class2():
-    pass
+               self.stc31_sensor.set_relative_humidity(sht40_humidity.value)
+               self.stc31_sensor.set_temperature(sht40_temperature.value)
+
+               # Read CO2 from STC31 sesnor
+               (co2_concentration, stc31_temperature) = self.stc31_sensor.measure_gas_concentration()
+
+               # Log CO2 concentration in Vol%, temperature in degree celsius,
+               # and humidity in %
+               self.logger.info({"CO2": co2_concentration, "temperature": sht40_temperature, 
+                                               "humidity": sht40_humidity})
+               self.pipe_sensor_data_out.send({"CO2": co2_concentration, "temperature": sht40_temperature, 
+                                               "humidity": sht40_humidity})
+               self.logger.info("data sent")
