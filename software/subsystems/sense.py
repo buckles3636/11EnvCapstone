1c3577f744fb995bf14750f5aa44f9646ef7350f
davidhjensen <dhjensen02@gmail.com>
2025-01-30 16:10:48 -0700

    added limiter to clamp delay values to positive numbers

--- software/subsystems/sense.py
+++ software/subsystems/sense.py
@@ -106,6 +106,6 @@ class Sensor(Subsystem):
                     self.pipe_sensor_data_out.send({"CO2": co2_concentration.value, "temperature": sht40_temperature.value, 
                                                   "humidity": sht40_humidity.value})
                     
-                    delta_ns = time.monotonic_ns() - cur_ns
+                    delta_ns = max(0, time.monotonic_ns() - cur_ns) #clamp if slow delay
                     print(f"SENSE:\t\tData read and transmitted in {delta_ns/1e6} ms")
                     time.sleep((self.T*1e6 - delta_ns)/1e9)
